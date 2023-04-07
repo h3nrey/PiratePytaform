8864f4b9b598eeb503e737fdd59ae9955f1a8c74
h3nrey <pedro.novaes.education@gmail.com>
2023-04-07 16:20:06 -0300

    basic setup

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-import pygame;
+import pygame , sys;
+
+pygame.init();
+screenW = 1200;
+screenH = 700;
+screen = pygame.display.set_mode((screenW, screenH));
+
+clock = pygame.time.Clock();
+FPS = 60;
+
+while True:
+    events = pygame.event.get();
+    for event in events:
+        if(event.type == pygame.QUIT):
+            pygame.quit();
+            sys.exit();
+
+    screen.fill("black");
+
+    pygame.display.update();
+    clock.tick(FPS);
